665f5d6af185f5f4fba67ef6ac4e878565c45c4f
Kurt Strecker <ksphotopro@gmail.com>
2019-02-03 23:44:12 -0500

    added clear button and bound F1 to print the text

--- pyprint_gui.py
+++ pyprint_gui.py
@@ -5,9 +5,12 @@ from printIt import send_text_to_printer
 def print_it():
     text_to_print=text_entry.get("1.0", END).strip()
     
-    send_text_to_printer(text_to_print)
+    if len(text_to_print) > 0:
+        send_text_to_printer(text_to_print)
 
-    print(text_to_print.strip())
+
+def clear():
+    text_entry.delete("1.0", END)
 
 root = Tk()
 root.title("PyPrint")
@@ -22,8 +25,9 @@ text_entry = Text(main_frame, width=40, height=10)
 text_entry.grid(column=0, row=0, sticky=(N, W), padx=10, pady=10)
 
 ttk.Button(main_frame, text="Print", command=print_it).grid(column=0, row=2, sticky=(W, E), padx=10, pady=10)
+ttk.Button(main_frame, text="Clear", command=clear).grid(column=0, row=3, sticky=(W, E), padx=10, pady=10)
 
-
+root.bind('<F1>', lambda e: print_it())
 #### Main Loop ####
 
 root.mainloop()
