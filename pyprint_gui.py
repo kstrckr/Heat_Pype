334c30dc8c7dd39ccbd9d2e99417e5243771c04f
Kurt Strecker <ksphotopro@gmail.com>
2019-02-05 23:28:53 -0500

    changed Text width to 48 chars

--- pyprint_gui.py
+++ pyprint_gui.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 from printIt import send_text_to_printer
 
 def print_it():
-    text_to_print=text_entry.get("1.0", END).strip()
+    text_to_print=text_entry.get("1.0", END)
     
     if len(text_to_print) > 0:
         send_text_to_printer(text_to_print)
@@ -21,7 +21,7 @@ main_frame.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-text_entry = Text(main_frame, width=40, height=10)
+text_entry = Text(main_frame, width=48, height=10)
 text_entry.grid(column=0, row=0, sticky=(N, W), padx=10, pady=10)
 
 ttk.Button(main_frame, text="Print", command=print_it).grid(column=0, row=2, sticky=(W, E), padx=10, pady=10)
