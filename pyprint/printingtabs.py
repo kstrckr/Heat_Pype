4222f590e7b60a6c0d47ea52cc79e0a613f9976b
kstrckr <ksphotopro@gmail.com>
2019-02-17 15:35:43 -0500

    moved footer to its own frame with buttons included

---
+++ pyprint/printingtabs.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import ttk
+
+import pyprint.texttab as tt
+import pyprint.rastertab as rt
+
+class Printing_Tabs(ttk.Notebook):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent)
+
+        self.text_tab = tt.Text_Tab(self)
+        self.raster_tab = rt.Raster_Tab(self)
+        self.add(self.text_tab, text="  Text  ")
+        self.add(self.raster_tab, text=" Raster ")
+        self.grid(kwargs)
+
+
+    def Get_Text_Input(self):
+        return self.text_tab.text_entry.get("1.0", tk.END)
+
+    def Clear_Text(self):
+        self.text_tab.text_entry.delete("1.0", tk.END)
